0cd05b0b0fd7c85ff531cec47956190a8e16cf0e
krupix <osiadacz93@gmail.com>
2016-06-13 22:30:52 +0200

    create project

---
+++ src/main/java/pl/krupix/mas/finalproject/model/DaoSerivce.java
@@ -0,0 +1,7 @@
+package pl.krupix.mas.finalproject.model;
+
+/**
+ * Created by krupix on 13.06.2016.
+ */
+public class DaoSerivce {
+}
---
+++ src/main/java/pl/krupix/mas/finalproject/starter/Starter.java
@@ -0,0 +1,7 @@
+package pl.krupix.mas.finalproject.starter;
+
+/**
+ * Created by krupix on 13.06.2016.
+ */
+public class Starter {
+}
